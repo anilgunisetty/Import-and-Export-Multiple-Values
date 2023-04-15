f5f0e2086fc98046b02c127f590bc3e1b8b1d952
anil <gunisettyanil143@gmail.com>
2023-04-15 06:21:31 +0000

    solution

---
+++ exportMultipleValues.mjs
@@ -0,0 +1,11 @@
+// Define array
+const myArray = ["camel", 265, true, "5.6"];
+
+// Define object
+const bulb = { watts: 10, type: "LED" };
+
+// Define function
+const multiplyByFour = (num) => num * 4;
+
+// Export the variables
+export { myArray, bulb, multiplyByFour };
---
+++ importMultipleValues.mjs
@@ -0,0 +1,7 @@
+// Import the variables
+import { myArray, bulb, multiplyByFour } from "./exportMultipleValues.mjs";
+
+// Use the imported variables
+console.log(myArray); // ["camel", 265, true, "5.6"]
+console.log(bulb); // {watts: 10, type: "LED"}
+console.log(multiplyByFour(5)); // 20
